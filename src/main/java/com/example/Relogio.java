8e23e210615670ee5cc78a8cf74a4d7606cb8be2
Victor Friche <vicfriche@gmail.com>
2023-08-23 22:13:59 -0300

    Teste do horario padrao/construtor

--- src/main/java/com/example/Relogio.java
+++ src/main/java/com/example/Relogio.java
@@ -2,4 +2,19 @@ package com.example;
 
 public class Relogio {
 
+	private int horas;
+    private int minutos;
+    private int segundos;
+
+    public Relogio() {
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
+        
+    }
+    
+    public String getHorario() {
+    	
+    }
+    
 }
--- src/test/java/com/example/RelogioTest.java
+++ src/test/java/com/example/RelogioTest.java
@@ -2,13 +2,21 @@ package com.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class RelogioTest {
+	
+private Relogio relogio;
+	    
+	@Before
+	public void setUp() {
+	    relogio = new Relogio();
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testHorarioPadraoConstrutor() {
+		assertEquals("00:00:00", relogio.getHorario());
 	}
 
 }
